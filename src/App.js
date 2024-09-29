54f92a9d5aadbab74e415dcf3d8e7ef4c84574ac
julietteuhln <julietteuhlen@gmail.com>
2024-09-29 21:31:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AllRoutes from './routes/AllRoutes';
 import Header from './components/Layout/Header';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/julietteuhln.github.io">
+      <HashRouter basename="/julietteuhln.github.io">
         <Header />
           <main>
             <AllRoutes />
           </main>
-        </BrowserRouter>
+    </HashRouter>
     </div>
   );
 }
